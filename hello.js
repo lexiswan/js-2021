be66e6176706811ddd61071c32d4f5876472581a
Lexi Swan <nacu.alexandra1@gmail.com>
2021-04-02 10:15:35 +0300

    java

---
+++ hello.js
@@ -0,0 +1,25 @@
+var time = 3000;
+time = time + 1000
+var message = "Bine ai venit!"
+var isLate = true
+
+if (isLate) {
+    alert("Go home")
+    
+}
+if (isLate) {
+    alert("Go home")
+    
+} else {
+    alert("Continue")
+    
+}
+
+setTimeout(welcome, time)
+
+
+function welcome() {
+    alert(message)
+    alert("Welcome!") 
+}
+
