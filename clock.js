f977f98702b45891c4c3f2239d23d084d9cb66cd
Lexi Swan <nacu.alexandra1@gmail.com>
2021-04-21 20:11:59 +0300

    clock

---
+++ clock.js
@@ -0,0 +1,24 @@
+var hour = new Date().getHours()
+hour = 18
+
+/*
+if (hour < 2) {
+    document.write("Somn usor!")
+} else if ( hour < 12) {
+    document.write("Buna dimineata!")
+} else if (hour < 18) {
+    document.write("Buna ziua!")
+} else (hour < 24) {
+    document.write("Buna seara!")
+}
+*/
+
+if (hour < 12 && hour > 1) {
+    document.write("Buna dimineata!")
+} else if (hour >= 12 && hour <18) {
+    document.write("Buna ziua!")
+} else if (hour >= 18 && hour <= 23) {
+    document.write("Buna seara!")
+} else {
+document.write("Somn usor!")
+}
