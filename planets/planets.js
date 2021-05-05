8988cbad9c96b740facef0d85c0870e0d742bdb5
Lexi Swan <nacu.alexandra1@gmail.com>
2021-05-05 21:15:57 +0300

    curs4

--- planets/planets.js
+++ planets/planets.js
@@ -1,9 +1,25 @@
-let planet = document.getElementById("redplanet")
+let button = document.getElementById("check")
+button.addEventListener("click", function () {
+    let planet = document.getElementById("redplanet")
 
-planet.innerText = "Aliens invasion"
-planet.classList.add("alert")
+    planet.innerText = "Aliens invasion"
+    planet.classList.add("alert")
 
-// planet.style.color = "red"
-// planet.style.fontWeight = "bold"
-// planet.remove()
+    // planet.style.color = "red"
+    // planet.style.fontWeight = "bold"
+    // planet.remove()
+})
 
+document.getElementById("reset")
+    .addEventListener("click", function () {
+        let planet = document.getElementById("redplanet")
+        planet.innerText = "Nothing to report"
+        planet.classList.remove("alert")
+    })
+
+
+document.getElementById("greenplanet")
+.addEventListener("click", function () {
+    document.getElementById("greenplanet") .classList.add("alert")
+    
+})
