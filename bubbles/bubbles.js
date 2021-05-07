f7d46072b3465ac431a62fe424d1f43721810b85
Lexi Swan <nacu.alexandra1@gmail.com>
2021-05-07 09:24:34 +0300

    curs5

--- bubbles/bubbles.js
+++ bubbles/bubbles.js
@@ -1,4 +1,4 @@
-var numberOfBubbles = 10
+var numberOfBubbles = 100
 for (let i = 0; i < numberOfBubbles; i++) {
     newBubble()
     
@@ -6,4 +6,13 @@ for (let i = 0; i < numberOfBubbles; i++) {
 function newBubble() {
     let bubble = document.createElement("div")
     bubble.classList.add("bubble")
+    let x = randomNumber(100)
+    let delay = randomNumber(3000)
+    bubble.style.left = x + "vw"
+    bubble.style.animationDelay = delay + "ms"
+    document.querySelector("body").appendChild(bubble)
+}
+
+function randomNumber(max) {
+    return Math.floor(Math.random() * max);
 }
