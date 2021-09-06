d684689c6b571c1db2a0723ee466a422bc563eca
Alexis <nacu.alexandra1@gmail.com>
2021-09-06 07:59:36 -0700

    vue

---
+++ vue/hello.js
@@ -0,0 +1,49 @@
+let app = new Vue({
+    //elementul pe care va rula aceasta aplicatie 
+    el: '#app',
+    data: {
+    counter: 0
+    },
+    methods: {
+      increment() {
+         this.counter++
+      }
+    },
+    computed: {
+         
+            time() {
+            let time = new Date()
+            return time.toTimeString()
+            },
+            message() {
+            let hour = new Date().getHours()
+            if (hour < 12 && hour > 1) {
+                return "Buna dimineata!"
+            } else if (hour >= 12 && hour <18) {
+                return "Buna ziua!"
+            } else if (hour >= 18 && hour <= 23) {
+                return "Buna seara!"
+            } else {
+            return "Somn usor!"
+            }
+        }
+    }
+})
+
+
+
+//Vanila JS
+
+ /*let time = new Date().toTimeString()
+document.querySelector("#now").innerText = time
+
+let message = "Test"
+document.querySelector("p:firstChild()").innerText = message */
+
+
+
+//let counter = 0
+//document.querySelector('.clicks').addEventListener('click' , function() {
+//   counter ++
+//   document.querySelector('.clicks').value = counter
+//  })
